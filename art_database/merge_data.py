ff43650c83955e0c3b3d3617d023b45c1cc3ed7e
andhri <andrea.hricikova@gmail.com>
2023-05-13 17:36:53 +0100

    created a function for merging data

--- art_database/merge_data.py
+++ art_database/merge_data.py
@@ -1,27 +1,37 @@
 import json
 
 # list of all filenames
-files = ['test_one', 'test_two']
-# files = ["sample_data_1840_2020_hands.json",
-#             "sample_data_dance.json",
-#             "sample_data_landscape.json",
-#             "sample_data_portrait.json",
-#             "sample_data_stilllife.json",
-#             "sample_data_v2.json",
-#             "sample_data.json"]
+files = ["art_database\sample_data_1840_2020_hands.json",
+         "art_database\sample_data_dance.json",
+         "art_database\sample_data_landscape.json",
+         "art_database\sample_data_portrait.json",
+         "art_database\sample_data_stilllife.json",
+         "art_database\sample_data_v2.json",
+         "art_database\sample_data.json",
+         "art_database\sample_data_abstract.json"
+          ]
 
 # merging extracted data files into one JSON
 def merge_json(filename):
-    total = list()
+    all_data = []
+    unique_ids= set()
+    # unique_dates = set()
     try:
-        for f1 in filename:
-            with open(f1, 'r') as infile:
-                total.extend(json.load(infile))
-
-        with open('counseling3.json', 'w') as output_file:
-            json.dump(total, output_file)
-    except ValueError:
-        "objectID" == "objectID"
+        for file in filename:
+            with open(file, 'r') as infile:
+                data = json.load(infile)
+                for item in data:
+                    if item['objectID'] not in unique_ids: # and item['objectEndDate'] not in unique_dates:
+                        unique_ids.add(item['objectID'])
+                        # unique_dates.add(item['objectEndDate'])
+                        all_data.append(item)
+        sorted_data = sorted(all_data, key=lambda d: d["objectID"])
+    except Exception as err:
+        return f"Error: {err}."
+    else:
+        with open('art_database\game_data.json', 'w+') as output_file:
+            json.dump(sorted_data, output_file, indent=4)
+            print(len(all_data))
 
 merge_json(files)
     
