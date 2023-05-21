e95931b2a0fefcaea6d88ad54ad1d21f5c7ed304
Mira Salawu <120687820+MiraxSal@users.noreply.github.com>
2023-05-21 11:48:34 +0100

    updated game_int_v3
    with some logic for opening add_info_popup

--- refactor_attempt_2/game_interface_v3.py
+++ refactor_attempt_2/game_interface_v3.py
@@ -5,16 +5,18 @@ from game_answers import GameAnswers
 from display import DisplayConfig
 import main_config
 from parent_interface import Parent
+from add_info_popup import OpenPopUp
 
 
 class GameInterface:
     def __init__(self, parent: Parent, art_game: ArtGame, art_options: ArtOptions,
-                 answers: GameAnswers, game_display: DisplayConfig):
+                 answers: GameAnswers, game_display: DisplayConfig, art_info: OpenPopUp):
         self.game_logic = art_game
         self.art_options = art_options
         self.answers = answers
         # self.stan_config = main_config
         self.game_display = game_display
+        self.art_info = art_info
 
         self.parent = parent
 
@@ -23,32 +25,48 @@ class GameInterface:
                           font=("Helvetica", 40, "bold"), fg=main_config.DEFAULT_FONT_COLOUR)
         self.title.grid(column=0, row=1, columnspan=2)
 
-        # self.add_info_1 = None
-        # self.add_info_2 = None
-
         self.generate_options()
 
         self.parent.window.mainloop()
 
+    def open_popup1(self):
+        print('pop-up for image 1')
+        OpenPopUp(self.parent)
+        # OpenPopUp(self.parent, title1, info1)
+
+    def open_popup2(self):
+        print('pop-up for image 2')
+        OpenPopUp(self.parent)
+        # OpenPopUp(self.parent, title2, info2)
+
     def generate_options(self):
+        self.game_display.border_1.config(background=main_config.THEME_COLOR, bd=10)
+        self.game_display.border_2.config(background=main_config.THEME_COLOR, bd=10)
+
         if self.game_logic.still_has_lives():
-            self.art_options.display_option_1()
 
             img1 = self.game_display.option_image(self.art_options.display_option_1())
             self.game_display.display_art_1.config(image=img1, command=self.option_1_answer)
             self.game_display.display_art_1.image = img1
 
-            self.art_options.display_option_2()
+            # self.art_info.label2.config(text=self.art_options.display_option_1()[1], wraplength=300)
+            # self.art_info.label4.config(text=self.art_options.display_option_1()[2], wraplength=300)
+            self.art_options.name_art_1.config(command=lambda: self.open_popup1())
 
             img2 = self.game_display.option_image(self.art_options.display_option_2())
-
             self.game_display.display_art_2.config(image=img2, command=self.option_2_answer)
             self.game_display.display_art_2.image = img2
+
+            # self.art_info.label2.config(text=self.art_options.display_option_2()[1], wraplength=300)
+            # self.art_info.label4.config(text=self.art_options.display_option_2()[2], wraplength=300)
+            self.art_options.name_art_2.config(command=lambda: self.open_popup2())
 
         else:
             print("You're Out of Lives!!")
             self.game_display.display_art_1.config(state="disabled")
             self.game_display.display_art_2.config(state="disabled")
+
+            self.title.config(text="You're Out Of Lives!", fg="red")
 
     def option_1_answer(self):
         self.answers.give_feedback(self.game_logic.check_players_choice("option_1"))
@@ -58,25 +76,7 @@ class GameInterface:
         self.answers.give_feedback(self.game_logic.check_players_choice("option_2"))
         self.parent.window.after(1000, self.generate_options)
 
-    # still working on this.....
-    # def open_popup(self):
-    #     InfoPopup().add_info_by_option()
-    #
-    # def add_info_by_option(self):
-    #     # self.add_info_1 = self.option_1["artistDisplayName", "country", "culture"]
-    #     # self.add_info_2 = self.option_2["artistDisplayName", "country", "culture"]
-    #
-    #     # this function isn't ready yet and still working on it....
-    #     if self.generate_options() = 'option_1':
-    #         self.add_info_1 = self.generate_options().option_1["artistDisplayName", "country", "culture"]
-    #         print(self.add_info_1)
-    #
-    #     elif self.generate_options().option_2:
-    #         self.add_info_2 = self.generate_options().option_2["artistDisplayName", "country", "culture"]
-    #         print(self.add_info_2)
-    #
-    # def close_popup(self):
-    #     self.destroy()
+
 
 
 
