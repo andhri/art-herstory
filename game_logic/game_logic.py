f7c2f0c0497c71971b3c5c2e76f11dc0f76c5eba
andhri <andrea.hricikova@gmail.com>
2023-05-27 14:20:26 +0100

    removed print statements

--- game_logic/game_logic.py
+++ game_logic/game_logic.py
@@ -1,6 +1,5 @@
 import random
 
-
 class ArtGame:
     """ Initialises the game logic """
     def __init__(self, database):
@@ -25,19 +24,10 @@ class ArtGame:
         self.option_id = self.database[self.current_id]["objectID"]
         self.option = [self.option_id == i['objectID'] for i in self.database].index(True)
 
-        # print('OPTION', self.current_id)
-        # print('OPTION', self.option_id)
-        # print('OPTION', self.option)
-        # print('LEFT', self.left_choice_index)
-        # print('RIGHT', self.right_choice_index)
-
         if position == 'left':
             print('LEFT')
             self.left_choice_index = self.option
         elif position == 'right' and self.option == self.left_choice_index:
-            # print('MIDDLE')
-            # print('???????', self.option)
-            # print('???????', self.left_choice_index)
             self.get_portrait_by_index('right')
         elif position == 'right':
             print('RIGHT')
@@ -47,7 +37,7 @@ class ArtGame:
     
     @staticmethod
     def check_year(year1, year2):
-        """Method which compares the years """
+        """Method which compares the years"""
         return year1 < year2
     
     def check_players_choice(self, user_answer):
@@ -57,26 +47,15 @@ class ArtGame:
 
         if (user_answer == "option_1" and self.check_year(option_1_year, option_2_year)) or (
                 user_answer == "option_2" and self.check_year(option_2_year, option_1_year)):
-            # print(self.database[self.left_choice_index]["objectEndDate"])
-            # print(self.database[self.right_choice_index]["objectEndDate"])
-            # print("before score", self.score)
-            # print("after score", self.score)
-
             self.score += 1
             return f"{user_answer}-correct"
 
         elif (user_answer == "option_1" and self.check_year(option_2_year, option_1_year)) or (
                 user_answer == "option_2" and self.check_year(option_1_year, option_2_year)):
-            # print(self.database[self.left_choice_index]["objectEndDate"])
-            # print(self.database[self.right_choice_index]["objectEndDate"])
-            # print("before lives", self.lives)
-            # print("after lives", self.lives)
-
             self.lives -= 1
             return f"{user_answer}-incorrect"
 
         # function for checking digits below
-    
     def replace_title_digits(self, art_title):
         """ Method to check if the art title has any integers; replaces any found with an 'X' to hide possible years """
         self.art_title = art_title
