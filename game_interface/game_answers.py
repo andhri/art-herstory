e49a1e187f4aa640ece5ce16528904577e048a92
andhri <andrea.hricikova@gmail.com>
2023-05-27 14:47:15 +0100

    removed print statements

--- game_interface/game_answers.py
+++ game_interface/game_answers.py
@@ -1,9 +1,9 @@
 from tkinter import *
+import pygame
 from game_logic.game_logic import ArtGame
 from game_interface.game_options import ArtOptions
-import pygame
 from game_interface.display import DisplayConfig
-import game_interface.main_config as main_config
+import game_interface.main_config as mc
 from game_interface.parent_interface import Parent
 
 
@@ -24,22 +24,18 @@ class GameAnswers:
         self.parent = parent
 
         self.lives_label = Label(self.parent.window, text=f"Lives: {self.game_logic.lives}",
-                                 background=main_config.THEME_COLOR, fg=main_config.TEXT_COLOUR,
-                                 width=10, font=main_config.font_body)
-        # self.lives_label.config(self.stan_config.gl)
+                                 background=mc.THEME_COLOR, fg=mc.TEXT_COLOUR,
+                                 width=10, font=mc.font_body)
         self.lives_label.grid(column=0, row=0)
 
         self.score_label = Label(self.parent.window, text=f"Score: {self.game_logic.score}",
-                                 background=main_config.THEME_COLOR, fg=main_config.TEXT_COLOUR,
-                                 width=10, font=main_config.font_body)
-        # self.score_label.config(self.stan_config.gl)
+                                 background=mc.THEME_COLOR, fg=mc.TEXT_COLOUR,
+                                 width=10, font=mc.font_body)
         self.score_label.grid(column=1, row=0)
 
 
     def give_feedback(self, players_choice):
         """Method to provide the user with visual and audio feedback based on their answers """
-        print("!!!This is the player's choice!:", players_choice)
-
         a = players_choice.split('-')
 
         if a[1] == "correct":
@@ -48,7 +44,7 @@ class GameAnswers:
             else:
                 self.game_display.border_2.config(background="green", bd=10)
 
-            sound_feedback(main_config.CORRECT_SOUND)
+            sound_feedback(mc.CORRECT_SOUND)
 
         elif a[1] == "incorrect":
             if a[0] == 'option_1':
@@ -56,7 +52,7 @@ class GameAnswers:
             else:
                 self.game_display.border_2.config(background="red", bd=10)
 
-            sound_feedback(main_config.INCORRECT_SOUND)
+            sound_feedback(mc.INCORRECT_SOUND)
 
         self.score_label.config(text=f"Score:{self.game_logic.score}")
         self.lives_label.config(text=f"Lives:{self.game_logic.lives}")
