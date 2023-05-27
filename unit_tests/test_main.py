eb9c3a351b48aba60689ff9da85e69f195f0af53
andhri <andrea.hricikova@gmail.com>
2023-05-27 19:31:45 +0100

    changed the test to work of temporary directory

--- unit_tests/test_main.py
+++ unit_tests/test_main.py
@@ -1,46 +1,17 @@
-from main import load_data
 import pytest
-import json
 
 #### writing tests for the main function ####
-@pytest.fixture
-def mocked_art_database(monkeypatch):
-    """Mock database output for testing"""
-    data =[
-        {
-        "objectID": 1,
-        "artistDisplayName": "Jane Anthony Davis",
-        "title": "Lady Seated in a Boston Rocker",
-        "primaryImage": "https://images.metmuseum.org/CRDImages/ad/original/ap66.242.14.jpg",
-        "objectDate": "1821",
-        "objectBeginDate": 1821,
-        "objectEndDate": 1855
-        },
-        {
-        "objectID": 2,
-        "artistDisplayName": "Sarah Goodridge",
-        "title": "Portrait of a Lady",
-        "primaryImage": "https://images.metmuseum.org/CRDImages/ad/original/ap68.222.25.jpg",
-        "objectDate": "1830",
-        "objectBeginDate": 1830,
-        "objectEndDate": 1830
-        }
-    ]
 
-    def mocked_return(*args, **kwargs):
-        return data
-    monkeypatch.setattr("random.randrange", mocked_return)
-    yield data
+DATA = 'data'
+def test_load_data(tmp_path):
+    """Test for the game to be loading a file from temp directory"""
+    d = tmp_path / "dict"
+    d.mkdir()
+    p = d / "test data"
+    p.write_text(DATA)
+    assert p.read_text() == DATA
+    assert len(list(tmp_path.iterdir())) == 1
 
-def test_load_data(mocked_art_database):
-    """Test for the game to be loading a file"""
-    expected_data = mocked_art_database
-
-    with open("art_database/game_data_clean.json", "w") as file:
-        json.dump(expected_data, file)
-    
-    data = load_data()
-    assert data == expected_data
 
 
 def test_load_data_missing_file():
